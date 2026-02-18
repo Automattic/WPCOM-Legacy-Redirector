59499d1fa7846f123a6332170846a6ee6c819740
dhusakovic <dejan.husakovic@automattic.com>
2026-02-18 14:17:00 +1100

    bot feedback

--- wpcom-legacy-redirector.php
+++ wpcom-legacy-redirector.php
@@ -41,17 +41,19 @@ const VERSION     = '2.0.0-alpha';
 if ( file_exists( __DIR__ . '/vendor/autoload.php' ) ) {
 	require_once __DIR__ . '/vendor/autoload.php';
 } else {
-	spl_autoload_register( function ( string $class ): void {
-		$prefix = 'Automattic\\LegacyRedirector\\';
-		if ( ! str_starts_with( $class, $prefix ) ) {
-			return;
+	spl_autoload_register(
+		function ( string $class_name ): void {
+			$prefix = 'Automattic\\LegacyRedirector\\';
+			if ( ! str_starts_with( $class_name, $prefix ) ) {
+				return;
+			}
+			$relative = substr( $class_name, strlen( $prefix ) );
+			$file     = __DIR__ . '/src/' . str_replace( '\\', '/', $relative ) . '.php';
+			if ( file_exists( $file ) ) {
+				require_once $file;
+			}
 		}
-		$relative = substr( $class, strlen( $prefix ) );
-		$file     = __DIR__ . '/src/' . str_replace( '\\', '/', $relative ) . '.php';
-		if ( file_exists( $file ) ) {
-			require_once $file;
-		}
-	} );
+	);
 }
 
 // Initialize the plugin.
